4834ca2767ee66dfcdffb57d52f00deab9d08401
beca2 <beca2@I4Y-DV4>
2017-01-03 13:31:26 +0100

    Calculo perimetro y area

---
+++ ejercicio13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radioCirculo;
+            double calculoPerimetro;
+            double calculoArea;
+
+            //Introducimos el radio del circulo
+            Console.WriteLine("Input the radius of the circle");
+            string element = Console.ReadLine();
+            bool comprobacion1 = double.TryParse(element, out radioCirculo);
+
+            //Calculamos el perimetro y el area
+            double pi = Math.PI;
+            calculoPerimetro = 2 * pi * radioCirculo;
+            calculoArea = pi * Math.Pow(radioCirculo, 2);
+
+            //Imprimimos el perimetro y el area
+            Console.WriteLine("Perimeter of circle: {0}", calculoPerimetro);
+            Console.WriteLine("Area of circle:{0}", calculoArea);
+
+            Console.ReadLine();
+        }
+    }
+}
