6f26972cbf3513021a9019801f3c6c59c9f70711
beca2 <beca2@I4Y-DV4>
2017-01-03 14:11:31 +0100

    Distinguir entre vocal, otro simbolo y un digito

---
+++ ejercicio14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Introducimos caracter
+            Console.WriteLine("Input a symbol:");
+            string caracter = Console.ReadLine();
+            Char caracter1=Convert.ToChar(caracter);
+
+            if (caracter1 == 'a' || caracter1 == 'e' || caracter1 == 'i' || caracter1 == 'o' || caracter1 == 'u')
+            {
+                Console.WriteLine("It's a lowercase vowel");
+            }
+            else if (caracter1 >= 'a' && caracter1 <= 'z' || caracter1 >= 'A' && caracter1 <= 'Z')
+            {
+                Console.WriteLine("It's any other simbol");
+            }
+            else
+            {
+                Console.WriteLine("It's a digit");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
