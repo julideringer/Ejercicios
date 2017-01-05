bc5a0edd5dcfae40b79cea865afd98edd59a46f7
beca2 <beca2@I4Y-DV4>
2017-01-05 10:26:35 +0100

    Ejercicio realizado buscar valor su frecuencia y multiplicacion

---
+++ Ejer3linq/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos variables
+            int tamanioArray;
+            int valoresArray;
+            int valorArray;
+
+            // Introducimos tamaño de nuestro array
+            Console.WriteLine("INput the number of elements of the array");
+            string tamanio = Console.ReadLine();
+            Int32.TryParse(tamanio, out tamanioArray);
+            int[] arrayEntrada = new int[tamanioArray];
+
+            //Introducimos valores de la array
+            for (int r = 0; r < tamanioArray; r++)
+            {
+                Console.WriteLine("Element -" + r);
+                string element = Console.ReadLine();
+                Int32.TryParse(element, out valoresArray);
+                arrayEntrada[r] = valoresArray;
+            }
+
+            var result = from n in arrayEntrada.Distinct()
+                         select new { number = n, valor = (from m in arrayEntrada where (m == n) select m).Count() };
+                 Console.ReadLine();
+
+            foreach(var juan in result)
+            {
+                Console.WriteLine(juan.number + " " + juan.valor*juan.number +" "+juan.valor);
+            }
+            Console.ReadLine();
+        }
+    }
+}
